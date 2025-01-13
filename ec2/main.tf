916fc2407171287386a8b02570181cfdd3bc2ec1
Paul Figueroa <sixtwentynine79@gmail.com>
2025-01-13 22:25:18 +0800

    Fix workdir

--- ec2/main.tf
+++ ec2/main.tf
@@ -24,9 +24,9 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "web_server" {
-  ami = "ami-0e48a8a6b7dc1d30b" # Amazon Linux 2023 singapore
+  ami           = "ami-0e48a8a6b7dc1d30b" # Amazon Linux 2023 singapore
   instance_type = "t2.micro"
-  
+
   subnet_id                   = "subnet-0f25ae65055ef6ce9"
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   associate_public_ip_address = true
